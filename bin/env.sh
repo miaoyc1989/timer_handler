cff10a197ec1b0a0a75f11f200a4cf090b88c3d8
miaoyc <miaoyongchao@haizhi.com>
2017-08-21 17:04:40 +0800

    add config

---
+++ bin/env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+HOME="$(cd `dirname "${BASH_SOURCE-$0}"`/..; pwd)"
+export PYTHONPATH=${HOME}/src
+export CONF=${HOME}/conf
--- server.py
+++ server.py
@@ -6,21 +6,23 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 
+from handler.hello_handler import HelloHandler
+
+
 from tornado.options import define, options
 
 define("port", default=8888, help="run on the given port", type=int)
 
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-    	self.write("Hello, vicky!")
+def load_handlers():
+    handlers = list()
+    handlers.append((r"/", HelloHandler))
+    return handlers
 
 
 def main():
     tornado.options.parse_command_line()
-    application = tornado.web.Application([
-                    (r"/", MainHandler),
-                    ])
+    application = tornado.web.Application(load_handlers())
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
